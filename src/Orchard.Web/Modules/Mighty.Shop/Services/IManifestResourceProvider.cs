c529e0888e8568e9a70d95a822780ac2f559ef74
MightyWing <mightywing@outlook.com>
2016-12-29 11:28:14 +0800

    添加功能

---
+++ src/Orchard.Web/Modules/Mighty.Shop/Services/IManifestResourceProvider.cs
@@ -0,0 +1,25 @@
+﻿using Orchard.UI.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mighty.Shop
+{
+    public class ResourceManifest : IResourceManifestProvider
+    {
+        public void BuildManifests(ResourceManifestBuilder builder)
+        {
+            // Create and add a new manifest
+            var manifest = builder.Add();
+
+            // Define a "common" style sheet
+            manifest.DefineStyle("Mighty.Shop.Common").SetUrl("common.css");
+
+            // Define the "shoppingcart" style sheet
+            manifest.DefineStyle("Mighty.Shop.ShoppingCart").SetUrl("shoppingcart.css").SetDependencies("Mighty.Shop.Common");
+        }
+    }
+
+}
